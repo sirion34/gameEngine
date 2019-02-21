b24bed14e35c32849015e0add170dc9691fd993b
Shuldyakov <33556795+sirion34@users.noreply.github.com>
2019-02-21 13:20:58 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,6 +7,26 @@
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(Resolution.x, Resolution.y), Title);
+    sf::View view( sf::FloatRect(0,0,1600,900));
+
+//        int counter = 0;
+//        int indicator = 0;
+
+    EventHandler eventHandler();
+    EventManager eventManger();
+    DrawManager drawManager();
+    while(window.isOpen())
+    {
+        eventManger().Execute(eventHandler(),window);
+
+//        float Velocity = 0.05;
+//        int Edge = 30;
+
+        window.clear();
+        drawManager().DrawTmpText(&window, "arial.ttf", TmpText);
+//        Scroll(&window, &view, 30, 0.05);
+        window.setView(view);
+        window.display();
+    }
 }
